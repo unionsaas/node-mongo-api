20ad38004481ee273a70a990c09fad73ec44c3c1
Zengs <zingsono@gmail.com>
2019-04-04 10:26:36 +0800

    1.0.0 node mongo mql

--- index.js
+++ index.js
@@ -1,78 +1,29 @@
-/**
- * Node Mongodb api Server
- */
-const path = require('path')
-
-//https://www.npmjs.com/package/koa
-const Koa = require('koa')
-const app = new Koa()
-
-function H(){
-
-}
+let MongoClient = require('mongodb').MongoClient
 
 /**
- * 配置多个Mongodb数据库连接
- * @param key
- * @param mongoConnect
+ *
+ * @param uris   K-V,K=dbName V=url
+ * @param options
+ * @returns {Promise<Function>}
  */
-H.prototype.mongo = (key='default', mongoConnect)=>{
-    global.mongo[key] = mongoConnect
-}
-
-/**
- * Koa
- * @param asyncFun
- */
-H.prototype.use = (asyncFun)=>{
-    app.use(asyncFun)
-}
-
-/**
- * 添加 Koa 路由
- * @param router
- */
-H.prototype.router = (router)=>{
-    app.use(router.routes()).use(router.allowedMethods())
-}
-
-/**
- * 启动服务
- * @param port  定义端口
- * @param koaStatic  指定静态目录
- */
-H.prototype.start = (port=10508, koaStatic)=>{
-    if(!koaStatic){
-        // https://www.npmjs.com/package/koa-static
-        koaStatic = require('koa-static')(path.join(dirname, '../../public'), {})
+module.exports = async (uris={}, options={})=>{
+    let dbs = { }
+    for(let k in uris){
+        let mongo = await MongoClient.connect(uris[k], options)
+        dbs[k] = (mongo)
     }
-    app.use(koaStatic)
-
-    let router = require('./koa-router')
-    app.use(router.routes()).use(router.allowedMethods())
-
-    app.listen(port)
-    console.log('listening on port ', port)
-}
-
-/**
- * 执行Mongodb操作
- * @param mql  执行操作的JSON对象
- */
-H.prototype.mongo = async (mql)=>{
-    console.log(mql)
-    let p = mql.params || []
-    let db = global.mongo[mql.db||'default'].db(mql.db||null)
-    let collection = await db.collection(mql.collection)
-    let rs = await collection[mql.method](...p)
-    if (mql.method == 'find') {
-        rs = mql.sort ? rs.sort(mql.sort) : rs
-        rs = mql.skip ? rs.skip(mql.skip) : rs
-        rs = mql.limit ? rs.limit(mql.limit) : rs
-        rs = await rs.toArray()
+    return async (mql)=>{
+        let p = mql.params || []
+        let db = dbs[mql.db].db()
+        let collection = await db.collection(mql.collection)
+        let rs = await collection[mql.method](...p)
+        if (mql.method == 'find') {
+            rs = mql.sort ? rs.sort(mql.sort) : rs
+            rs = mql.skip ? rs.skip(mql.skip) : rs
+            rs = mql.limit ? rs.limit(mql.limit) : rs
+            rs = await rs.toArray()
+        }
+        return rs
     }
-    //Mongo响应结果
-    console.log(rs)
-}
 
-module.exports = new H()
+}
